ca1d8ab12e9ad86892b9a8c205785bcd0fa2e5f3
Jakob <jakob.grannas@gmail.com>
2014-07-28 21:38:04 +0200

    Initial code commit. Added Atomic Design SASS structure and removed some stuff I didn't need

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+var gulp = require('gulp'),
+	clean = require('gulp-clean'),
+	order = require('gulp-order'),
+	sass = require('gulp-sass'),
+	concat = require('gulp-concat'),
+	rename = require('gulp-rename'),
+	imagemin = require('gulp-imagemin'),
+	minifyCSS = require('gulp-minify-css'),
+	uglify = require('gulp-uglify'),
+    bourbon = require('node-bourbon'),
+	paths = {
+		scripts: './js/*.js',
+		sass: {
+			src: './sass/**/*.scss',
+			dest: './dist/css/'
+		},
+		images: './images/**/*',
+		fonts: './fonts/**/*'
+	};
+
+gulp.task('scripts', function() {
+	return gulp.src(paths.scripts)
+		.pipe(order([
+			'*.js',
+			'main.js'
+		]))
+		.pipe(concat('scripts.js'))
+		.pipe(gulp.dest('./dist/js/'))
+		.pipe(uglify())
+		.pipe(rename(function(path) {
+			path.extname = '.min.js';
+		}))
+		.pipe(gulp.dest('./dist/js/'));
+});
+
+gulp.task('sass', function() {
+	return gulp.src(paths.sass.src)
+		.pipe(sass({
+			imagePath: '../images',
+			includePaths: bourbon.with('utils', 'atoms', 'molecules', 'organisms', 'templates')
+		}))
+		.pipe(gulp.dest(paths.sass.dest))
+		.pipe(minifyCSS())
+		.pipe(rename(function(path) {
+			path.extname = '.min.css';
+		}))
+		.pipe(gulp.dest(paths.sass.dest));
+});
+
+gulp.task('images', function() {
+	return gulp.src(paths.images)
+		.pipe(imagemin())
+		.pipe(gulp.dest('./dist/images/'));
+});
+
+gulp.task('fonts', function() {
+	return gulp.src(paths.fonts)
+		.pipe(gulp.dest('./dist/fonts/'));
+});
+
+gulp.task('clean', function() {
+	return gulp.src(['dist/css', 'dist/js'], {read: false})
+		.pipe(clean());
+});
+
+gulp.task('watch', function() {
+	gulp.watch(paths.scripts, ['scripts']);
+	gulp.watch(paths.images, ['images']);
+	gulp.watch(paths.sass.src, ['sass']);
+});
+
+gulp.task('default', ['clean'], function() {
+	gulp.start('scripts', 'images', 'fonts', 'sass');
+});
