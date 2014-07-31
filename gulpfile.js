71dc04136d4e17b2e442ac8c0a9ce69229696552
Jakob <jakob.grannas@gmail.com>
2014-08-01 00:42:51 +0200

    WIP. Adding testimonials section

--- gulpfile.js
+++ gulpfile.js
@@ -48,7 +48,7 @@ gulp.task('sass', function() {
 		.pipe(gulp.dest(paths.sass.dest))
         .pipe(autoprefixer('last 2 version', '> 1%', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4', { cascade: true }))
 		.pipe(minifyCSS())
-		.pipe(rename({ suffix: 'min' }))
+		.pipe(rename({ suffix: '.min' }))
 		.pipe(gulp.dest(paths.sass.dest));
 });
 
