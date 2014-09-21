9f1c523e5c7926b00033ae8cd3d059c6278058cd
Jakob Grannas <jakob.grannas@vaimo.com>
2014-09-21 02:02:21 +0200

    Added new banner design

--- _banner.php
+++ _banner.php
@@ -1,5 +1,5 @@
 <header class="site-header clearfix" id="site-header">
-	<a href="#" class="logo"><span class="logo__text">Hjärtesorg</span></a>
+	<a href="#" class="logo site-header__logo"><span class="logo__text">Hjärtesorg</span></a>
 
 	<nav class="banner__menu">
 		<?php
@@ -20,11 +20,15 @@
 
 <div id="banner" class="banner">
 	<div class="banner__slogan">
-		<?php echo ipSlot('text', array('id' => 'banner-slogan', 'tag' => 'div',
-			'default' => __('Sorgbearbetning för barn och vuxna', 'Hjartesorg', false), 'class' => 'banner__slogan-text')); ?>
+		<?php echo ipSlot('text', array('id' => 'banner-slogan-row1', 'tag' => 'div',
+			'default' => __('Sorgbearbetning', 'Hjartesorg', false), 'class' => 'banner__slogan-text slogan__row1')); ?>
+
+		<?php echo ipSlot('text', array('id' => 'banner-slogan-row2', 'tag' => 'div',
+			'default' => __('för barn & vuxna', 'Hjartesorg', false), 'class' => 'banner__slogan-text slogan__row2')); ?>
 
 		<div class="banner__divider"><span class="divider__symbol"></span></div>
+
 		<?php echo ipSlot('text', array('id' => 'tell-me-more', 'tag' => 'a',
-			'default' => __('Berätta mer!','Hjartesorg'), 'class' => 'banner__button-important')); ?>
+			'default' => __('Berätta mer!','Hjartesorg'), 'class' => 'banner__button-important button-positive-filled')); ?>
 	</div>
 </div>
