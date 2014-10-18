c8814808a0e94ae7a472c490272fb052e1d3d3e9
Jakob Grannas <jakob.grannas@vaimo.com>
2014-10-19 00:00:55 +0200

    Something went wrong in the previous commit, so I'm fixing it

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,5 +13,31 @@
 			headroom  = new Headroom(headerEl, options);
 
 		headroom.init();
+
+		$(document).on('click', '.type-heart .rate', rate);
+		$( '.type-heart .rate-label').on({
+			mouseenter: rateHover,
+			mouseleave: rateHover
+		});
+	}
+
+	function rate() {
+		var checkedEl = $(this);
+		checkedEl.prevAll().addClass('checked');
+		checkedEl.nextAll().removeClass('checked');
+	}
+
+	function rateHover () {
+		var el = $('#' + $(this).attr('for')),
+			checkedEls = $('.type-heart .rate:checked');
+
+		if(checkedEls.length > 0 && el.is(':last-child')) {
+			el.nextAll().addClass('hover');
+			return false;
+		}
+
+		el.toggleClass('hover');
+		el.prevAll().toggleClass('hover');
+		el.nextAll().removeClass('hover');
 	}
 })();
