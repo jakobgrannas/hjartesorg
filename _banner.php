debcbb5692543514c3f96e8927a403ab57580822
Jakob Grannas <jakob.grannas@vaimo.com>
2015-02-19 12:30:44 +0100

    Removed 'For barn och vuxna' text temporarily

--- _banner.php
+++ _banner.php
@@ -3,8 +3,8 @@
 		<?php echo ipSlot('text', array('id' => 'banner-slogan-row1', 'tag' => 'div',
 			'default' => __('Sorgbearbetning', 'Hjartesorg', false), 'class' => 'banner__slogan-text slogan__row1')); ?>
 
-		<?php echo ipSlot('text', array('id' => 'banner-slogan-row2', 'tag' => 'div',
-			'default' => __('för barn & vuxna', 'Hjartesorg', false), 'class' => 'banner__slogan-text slogan__row2')); ?>
+		<?php //echo ipSlot('text', array('id' => 'banner-slogan-row2', 'tag' => 'div',
+			//'default' => __('för barn & vuxna', 'Hjartesorg', false), 'class' => 'banner__slogan-text slogan__row2')); ?>
 
 		<div class="banner__divider"><span class="divider__symbol"></span></div>
 
